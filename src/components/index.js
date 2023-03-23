38e994f9058495c4fd8c96247f7d1d91d376fc77
GowthamreddyZEN <g.sugurpatinti@zensar.com>
2023-03-23 12:23:21 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Bussiness from "./Bussiness";
+import Clients from "./Clients";
+import Cta from "./Cta";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testiminials from "./Testiminials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Bussiness,
+  Clients,
+  Cta,
+  Stats,
+  Footer,
+  Testiminials,
+  Hero,
+};
